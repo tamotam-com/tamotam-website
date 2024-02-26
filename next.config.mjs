f9cdfc7e532c0ed125380113de4cce10a87e916b
Daniel Danielecki <daniel.danielecki@mail.com>
2024-02-26 09:42:31 +0100

    Versioning untracked files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  distDir: "out",
+};
 
 export default nextConfig;
